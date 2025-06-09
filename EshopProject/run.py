695f9365f038a548f5bcb6dc1fdae673bf5b2266
mimigrant123 <112561545+mimigrant123@users.noreply.github.com>
2025-06-09 16:37:13 +0300

    Delete EshopProject directory

--- EshopProject/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app, create_tables_and_seed
-
-app = create_app()
-
-create_tables_and_seed(app) # Manually create tables & seed before the first request ever comes in:
-
-if __name__ == '__main__':
-    app.run(debug=True)
